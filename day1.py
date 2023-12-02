5af454eedd14102ffbde94bb6f2fcbf95e1f1d77
Ovannes Galstian <ovannes@fundomate.com>
2023-12-02 14:45:05 -0800

    Simplify

--- day1.py
+++ day1.py
@@ -1,42 +1,18 @@
+import sys
+
 def part1(data):
-    _sum = 0
-    for line in data:
-        digits = [int(i) for i in line if i.isdigit()]
-        if len(digits) > 0:
-            _sum += digits[0] * 10 + digits[-1]
-    return _sum
+    return sum((int(digits[0]) * 10 + int(digits[-1]) for digits in ([i for i in line if i.isdigit()] for line in data) if len(digits) > 0))
 
 def part2(data):
-    mappings = {
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9",
-    }
-
-    new_data = [[x if (x := "".join([v for k, v in mappings.items() if line[i:].startswith(k)])) else line[i] for i in range(len(line))] for line in data]
-    return part1(new_data)
+    mappings = { "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+    return part1([[x if (x := "".join([v for k, v in mappings.items() if line[i:].startswith(k)])) else line[i] for i in range(len(line))] for line in data])
 
 def execute(file_path):
-    total_sum1 = 0
-    total_sum2 = 0  
-    with open(file_path, 'r') as file:
-        total_sum1 = part1(file)
-
-    with open(file_path, 'r') as file:
-        total_sum2 = part2(file)
-
-    print("Answer 1: ", total_sum1)
-    print("Answer 2: ", total_sum2)
-
+    print("Answer 1: ", part1(open(file_path, 'r')))
+    print("Answer 2: ", part2(open(file_path, 'r')))
 
 print("EXAMPLE")
-execute('./data/day1/example.txt')
+execute('./data/'+sys.argv[0].split(".py")[0]+'/example.txt')
 
 print("SOLUTION")
-execute('./data/day1/data.txt')
+execute('./data/'+sys.argv[0].split(".py")[0]+'/data.txt')
