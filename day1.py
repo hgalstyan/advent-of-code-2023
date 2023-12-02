467d92d7a76525c36e56a15cac0e637c0128e0b9
Ovannes Galstian <ovannes@fundomate.com>
2023-12-02 14:07:17 -0800

    Day2

--- day1.py
+++ day1.py
@@ -1,11 +1,9 @@
-file_path = './data/day1.txt'
-# file_path = './data/example1.txt'
-
 def part1(data):
     _sum = 0
     for line in data:
         digits = [int(i) for i in line if i.isdigit()]
-        _sum += digits[0] * 10 + digits[-1]
+        if len(digits) > 0:
+            _sum += digits[0] * 10 + digits[-1]
     return _sum
 
 def part2(data):
@@ -24,15 +22,21 @@ def part2(data):
     new_data = [[x if (x := "".join([v for k, v in mappings.items() if line[i:].startswith(k)])) else line[i] for i in range(len(line))] for line in data]
     return part1(new_data)
 
-total_sum1 = 0
-total_sum2 = 0
+def execute(file_path):
+    total_sum1 = 0
+    total_sum2 = 0  
+    with open(file_path, 'r') as file:
+        total_sum1 = part1(file)
 
-with open(file_path, 'r') as file:
-    total_sum1 = part1(file)
+    with open(file_path, 'r') as file:
+        total_sum2 = part2(file)
 
-with open(file_path, 'r') as file:
-    total_sum2 = part2(file)
+    print("Answer 1: ", total_sum1)
+    print("Answer 2: ", total_sum2)
 
-print("Answer 1: ", total_sum1)
-print("Answer 2: ", total_sum2)
 
+print("EXAMPLE")
+execute('./data/day1/example.txt')
+
+print("SOLUTION")
+execute('./data/day1/data.txt')
