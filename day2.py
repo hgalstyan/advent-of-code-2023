d008b285e1c3297a0fe8ef5260d6cc2733dc69da
Ovannes Galstian <ovannes@fundomate.com>
2023-12-02 14:41:29 -0800

    Simplify

--- day2.py
+++ day2.py
@@ -1,61 +1,25 @@
 import sys
 from collections import defaultdict 
+from functools import reduce
 
 target = defaultdict(int, {'red': 12, 'green': 13, 'blue': 14})
 
 def parse(data):
-    result = []
-    for line in data:
-        _, result_str = line.strip().split(': ')
-        max_numbers = defaultdict(int, {'red': 0, 'green': 0, 'blue': 0})
-        for game_result in result_str.split("; "):
-            for r in game_result.split(", "):
-                num, color = r.split(' ')
-                if(max_numbers[color] < int(num)):
-                    max_numbers[color] = int(num)
-        result.append(max_numbers)
-    return result
+    return [{color: max(int(num) if color == c else 0 for game_result in line.strip().split(': ')[-1].split("; ") for num, c in (r.split(' ') for r in game_result.split(", "))) for color in ['red', 'green', 'blue']} for line in data]
 
 
 def part1(data):
-    result = 0
-    max_result_list = parse(data)
-    for index, max_result in enumerate(max_result_list):
-        is_valid = True
-        for _, color in enumerate(max_result):
-            if(target[color] < max_result[color]):
-                is_valid = False
-        if is_valid:
-            result += index + 1
-    return result
+    return sum(index + 1 for index, max_result in enumerate(parse(data)) if all(target[color] >= max_result[color] for color in max_result))
 
 def part2(data):
-    result = 0
-    max_result_list = parse(data)
-    for index, max_result in enumerate(max_result_list):
-        power = 1
-        for _, color in enumerate(max_result):
-            power *= max_result[color]
-        result += power
-    return result
+    return sum(reduce(lambda x, y: x * y, (num for _ ,num in max_result.items())) for max_result in parse(data))
 
 def execute(file_path):
-    total_sum1 = 0
-    total_sum2 = 0  
-    with open(file_path, 'r') as file:
-        total_sum1 = part1(file)
-
-    with open(file_path, 'r') as file:
-        total_sum2 = part2(file)
-
-    print("Answer 1: ", total_sum1)
-    print("Answer 2: ", total_sum2)
-
-
-filePath = sys.argv[0].split(".py")[0]
+    print("Answer 1: ", part1(open(file_path, 'r')))
+    print("Answer 2: ", part2(open(file_path, 'r')))
 
 print("EXAMPLE")
-execute('./data/'+filePath+'/example.txt')
+execute('./data/'+sys.argv[0].split(".py")[0]+'/example.txt')
 
 print("SOLUTION")
-execute('./data/'+filePath+'/data.txt')
+execute('./data/'+sys.argv[0].split(".py")[0]+'/data.txt')
